c87ae3e94144cc35633d9d539f2a135dd9e96c47
Olena Turcheniuk <111610861+HelensBadCode@users.noreply.github.com>
2024-03-18 18:56:30 +0200

    Update SmartTablePage.js

--- cypress/e2e/homework15/tables/SmartTablePage.js
+++ cypress/e2e/homework15/tables/SmartTablePage.js
@@ -38,8 +38,8 @@ export default class SmartTablePage{
     this.container.find('.nb-close').should("be.visible").click();
   }
 
-  editRecord(raw) {
-    this.container.find('.nb-edit').eq(raw).click();
+  editRecord(row) {
+    this.container.find('.nb-edit').eq(row).click();
   }
 
   deleteRecord() {
